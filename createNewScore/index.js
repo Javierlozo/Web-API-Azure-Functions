fa2f21f8909601f9e85906b39fcc3ec0d324ba03
Luis Lozoya <luisloart@gmail.com>
2022-04-07 12:37:36 -0400

    Updated score Item and connection with DB

--- createNewScore/index.js
+++ createNewScore/index.js
@@ -1,11 +1,29 @@
+require('dotenv').config();
+
+const ENDPOINT = process.env.ENDPOINT
+const KEY = process.env.KEY
+
 const CosmosClient = require("@azure/cosmos").CosmosClient;
 
+const client = new CosmosClient({ ENDPOINT, KEY})
 
+const databaseId = "ScoreDB";
+const containerId = "Scores";
 
-const client = new CosmosClient({ endpoint, key})
-
+const database = client.database(databaseId);
+const container = database.container(containerId);
 
 module.exports = async function (context, req) {
+
+    let scoreItem = {
+        "name": req.query.name,
+        "score": 0
+    }
+
+    const { resource: createdItem } = await container.items.create(scoreItem); 
+
+    const responseMessage = "Done!"
+
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
